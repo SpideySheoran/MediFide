f2a43b4203728cb4767ed720508584b61979167d
SpideySheoran <f20180304@hyderabad.bits-pilani.ac.in>
2021-03-17 21:15:36 +0530

    u

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
 });
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const DATABASE_CONNECTION = process.env.MONGODB_URI;
 
 mongoose.connect(DATABASE_CONNECTION, {
