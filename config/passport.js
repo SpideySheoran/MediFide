076ed3207221f0fffd505e86ef848fc87ee73963
unknown <Vishesh Badjatya>
2021-04-02 23:06:35 +0530

    sprint 2 part 3

--- config/passport.js
+++ config/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/google/callback",
+      callbackURL: "/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       Patient.findOne({ email: profile.emails[0].value }).then(
