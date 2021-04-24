8e01bdfe7c5bb21b32c229b40a734241f5b35e6f
karlriddz <67477722+karlriddz@users.noreply.github.com>
2021-04-24 19:02:22 +0530

    Admin changes 2

---
+++ client/src/components/Admin/RemoveDoctor.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button
+  } from 'reactstrap';
+
+class RemoveDoc extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+          <div className="container">
+              <div className="row">
+                <div className="col-4">
+                    <Card>
+                            <CardImg top width="100%" src="/assets/images/1.png" alt="Doc" />
+                            <CardBody>
+                                <CardTitle tag="h3">Dr. Suresh Rana</CardTitle>
+                                <a href="./Doc1">
+                                    <Button
+                                        className="font-weight-bold"
+                                        color="info"
+                                        size="lg"
+                                        block>
+                                
+                                        Remove
+                                    </Button>
+                                </a>
+                            </CardBody>
+                    </Card>
+                </div>
+                <div className="col-4">
+                    <Card>
+                            <CardImg top width="100%" src="/assets/images/2.jpg" alt="Doc" />
+                            <CardBody>
+                                <CardTitle tag="h3">Dr. SMeena</CardTitle>
+                                <a href="./Doc2">
+                                    <Button
+                                        className="font-weight-bold"
+                                        color="info"
+                                        size="lg"
+                                        block>
+                                
+                                        Remove
+                                    </Button>
+                                </a>
+                            </CardBody>
+                    </Card>
+                </div>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default RemoveDoc;
