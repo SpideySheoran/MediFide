40833ffe3a806a9a1b883a43cd2d2b90aba61756
karlriddz <67477722+karlriddz@users.noreply.github.com>
2021-04-25 15:40:08 +0530

    Prescription

---
+++ client/src/components/DoctorsProfile/FooterComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Footer(props) {
+    return(
+    <div className="w-100 footer mt-5" >
+        <div className="container">
+            <div className="row justify-content-center">             
+                {/*<div className="col-4 offset-1 col-sm-2">
+                    <h5>Links</h5>
+                    <ul className="list-unstyled">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="contactus.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>
+		              121, Clear Water Bay Road<br />
+		              Clear Water Bay, Kowloon<br />
+		              HONG KONG<br />
+		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
+		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
+		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                         medifide@hospital.com</a>
+                    </address>
+                </div>*/}
+                
+            </div>
+            <div className="row justify-content-center ">             
+                <div className="col-auto">
+                    <p>© Copyright 2018 Medifide</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Footer;
